f6c5ce5d57b86dfa2c77e2d37d1d5c68d7a3a101
kabe1725 <kabe1725@e3315-01-1707.ad.hj.se>
2018-05-09 15:51:43 +0200

    Progress

--- Src/LCDHelper.c
+++ Src/LCDHelper.c
@@ -105,13 +105,13 @@ void TextLCD_Puts(TextLCDType *lcd, char *string){
 void TextLCD_Putint(TextLCDType *lcd, uint32_t vrefCalc){
 
 	int n = log10(vrefCalc) + 1;
-	   int i;
-	   char *numberArray = calloc(n, sizeof(char));
-	   for ( i = 0; i < n; ++i, vrefCalc /= 10 ){
-	       numberArray[i] = vrefCalc % 10;
-	   }
+	int i;
+	char *numberArray = calloc(n, sizeof(char));
+	for ( i = 0; i < n; ++i, vrefCalc /= 10 ){
+		numberArray[i] = vrefCalc % 10;
+	}
 
-	   TextLCD_Puts(&lcd, numberArray);
+	TextLCD_Puts(&lcd, numberArray);
 
 }
 
